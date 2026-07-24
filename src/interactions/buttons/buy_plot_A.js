55a6f077cc987e1e6503470877ea8be2b59a2b24
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-24 19:29:54 -0400

    Update buy_plot_A.js

--- src/interactions/buttons/buy_plot_A.js
+++ src/interactions/buttons/buy_plot_A.js
@@ -1,4 +1,41 @@
+import { getPomp, removePomp } from '../../utils/points.js';
+
 export default {
+
+    customId: 'buy_plot_C',
+
+    async execute(interaction) {
+
+        const price = 500;
+
+        const points = await getPomp(
+            interaction.guild.id,
+            interaction.user.id
+        );
+
+        if (points < price) {
+            return interaction.reply({
+                content: `❌ No tienes suficientes Pomp. Necesitas **${price}** y tienes **${points}**.`,
+                ephemeral: true
+            });
+        }
+
+
+        await removePomp(
+            interaction.guild.id,
+            interaction.user.id,
+            price
+        );
+
+
+        await interaction.reply({
+            content: `🟩 Compraste la Parcela C (25x25).\n💎 Gastaste **${price} Pomp**.`,
+            ephemeral: true
+        });
+
+    }
+
+};export default {
 
     customId: 'buy_plot_A',
 
