e5f06e64b5b7cff8792225f3aa1bcdddf26006d3
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-24 14:25:52 -0400

    Create embeds.js

---
+++ src/utils/embeds.js
@@ -0,0 +1,50 @@
+import { EmbedBuilder } from 'discord.js';
+import { BotConfig } from '../config/bot.js';
+
+export function createEmbed(options = {}) {
+    const embed = new EmbedBuilder();
+
+    if (options.title) embed.setTitle(options.title);
+    if (options.description) embed.setDescription(options.description);
+
+    embed.setColor(
+        options.color || BotConfig.embeds.colors.primary
+    );
+
+    if (options.footer) {
+        embed.setFooter({
+            text: options.footer
+        });
+    }
+
+    if (options.thumbnail) {
+        embed.setThumbnail(options.thumbnail);
+    }
+
+    if (options.image) {
+        embed.setImage(options.image);
+    }
+
+    return embed;
+}
+
+export function successEmbed(description) {
+    return createEmbed({
+        description,
+        color: BotConfig.embeds.colors.success
+    });
+}
+
+export function errorEmbed(description) {
+    return createEmbed({
+        description,
+        color: BotConfig.embeds.colors.error
+    });
+}
+
+export function infoEmbed(description) {
+    return createEmbed({
+        description,
+        color: BotConfig.embeds.colors.info
+    });
+}
