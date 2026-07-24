6a1dffbf0527086c816492f39b61b744fc088ac1
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-24 19:46:44 -0400

    Update missions.js

--- src/utils/missions.js
+++ src/utils/missions.js
@@ -1,13 +1,15 @@
 import { getFromDb, setInDb } from './database/wrapper.js';
 
-function getMissionKey(guildId, id) {
+
+function missionKey(guildId, id) {
     return `mission:${guildId}:${id}`;
 }
 
 
 export async function createMission(guildId, id, data) {
+
     await setInDb(
-        getMissionKey(guildId, id),
+        missionKey(guildId, id),
         data
     );
 
@@ -16,18 +18,20 @@ export async function createMission(guildId, id, data) {
 
 
 export async function getMission(guildId, id) {
+
     return await getFromDb(
-        getMissionKey(guildId, id),
+        missionKey(guildId, id),
         null
     );
+
 }
 
 
 export async function updateMission(guildId, id, data) {
+
     await setInDb(
-        getMissionKey(guildId, id),
+        missionKey(guildId, id),
         data
     );
 
-    return data;
 }
