b393985b065efa23bebc3989ced84cb5a7556f12
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-24 18:53:15 -0400

    Update postgresDatabase.js

--- src/utils/postgresDatabase.js
+++ src/utils/postgresDatabase.js
@@ -1,11 +1,29 @@
+import pg from "pg";
+
+const { Pool } = pg;
+
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+    ssl: process.env.POSTGRES_SSL ? { rejectUnauthorized: false } : false
+});
+
 export const pgDb = {
-    insertVerificationAudit: async () => {
+    query: async (text, params = []) => {
+        const result = await pool.query(text, params);
+        return result;
+    },
+
+    connect: async () => {
+        await pool.query("SELECT 1");
         return true;
     },
-    query: async () => {
-        return [];
-    },
-    connect: async () => {
+
+    insertVerificationAudit: async (record) => {
+        await pool.query(
+            `INSERT INTO verification_audit (data)
+             VALUES ($1)`,
+            [record]
+        );
         return true;
     }
 };
