702edc41fc46b2bda6bb1424558f4868165492fe
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-24 19:51:58 -0400

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,82 +1,37 @@
-import { Events } from 'discord.js';
-import { logger } from '../utils/logger.js';
-import { handleInteractionError } from '../utils/errorHandler.js';
+if (interaction.isButton()) {
 
-export default {
-  name: Events.InteractionCreate,
+    const [customId, ...args] =
+        interaction.customId.split(':');
 
-  async execute(interaction, client) {
-    try {
-
-      if (interaction.isChatInputCommand()) {
-
-        const command = client.commands.get(
-          interaction.commandName
-        );
-
-        if (!command) return;
-
-        await command.execute(
-          interaction,
-          client
-        );
-
-        return;
-      }
+    let button = client.buttons.get(customId);
 
 
-      if (interaction.isButton()) {
+    if (!button) {
 
-        const [customId, ...args] =
-          interaction.customId.split(':');
+        for (const btn of client.buttons.values()) {
 
-        const button =
-          client.buttons.get(customId);
+            if (
+                btn.customIdPrefix &&
+                customId.startsWith(btn.customIdPrefix)
+            ) {
+                button = btn;
+                break;
+            }
 
-        if (!button) return;
-
-        await button.execute(
-          interaction,
-          client,
-          args
-        );
-
-        return;
-      }
-
-
-      if (interaction.isStringSelectMenu()) {
-
-        const [customId, ...args] =
-          interaction.customId.split(':');
-
-        const menu =
-          client.selectMenus.get(customId);
-
-        if (!menu) return;
-
-        await menu.execute(
-          interaction,
-          client,
-          args
-        );
-
-        return;
-      }
-
-
-    } catch (error) {
-
-      logger.error(
-        'Interaction error:',
-        error
-      );
-
-      await handleInteractionError(
-        interaction,
-        error
-      );
+        }
 
     }
-  }
-};
+
+
+    if (!button) return;
+
+
+    await button.execute(
+        interaction,
+        client,
+        args
+    );
+
+
+    return;
+}
