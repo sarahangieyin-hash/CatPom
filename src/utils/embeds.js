36563ab376e1400f5bfb0221ecc0c5e7aa451063
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-24 16:29:21 -0400

    Update embeds.js

--- src/utils/embeds.js
+++ src/utils/embeds.js
@@ -3,19 +3,24 @@ import { BotConfig } from '../config/bot.js';
 
 
 export function createEmbed(options = {}) {
+
     const embed = new EmbedBuilder();
+
 
     if (options.title) {
         embed.setTitle(options.title);
     }
 
+
     if (options.description) {
         embed.setDescription(options.description);
     }
 
+
     embed.setColor(
-        options.color || BotConfig.embeds.colors.primary
+        options.color ?? BotConfig.embeds.colors.primary
     );
+
 
     if (options.footer) {
         embed.setFooter({
@@ -23,58 +28,88 @@ export function createEmbed(options = {}) {
         });
     }
 
+
     if (options.thumbnail) {
         embed.setThumbnail(options.thumbnail);
     }
+
 
     if (options.image) {
         embed.setImage(options.image);
     }
 
+
     return embed;
 }
 
 
+
 export function successEmbed(description) {
+
     return createEmbed({
+
         description,
+
         color: BotConfig.embeds.colors.success
+
     });
+
 }
+
 
 
 export function errorEmbed(description) {
+
     return createEmbed({
+
         description,
+
         color: BotConfig.embeds.colors.error
+
     });
+
 }
+
 
 
 export function infoEmbed(description) {
+
     return createEmbed({
+
         description,
+
         color: BotConfig.embeds.colors.info
+
     });
+
 }
+
 
 
 export function warningEmbed(description) {
+
     return createEmbed({
+
         description,
-        color: BotConfig.embeds.colors.warning || "#FEE75C"
+
+        color: '#FEE75C'
+
     });
+
 }
+
 
 
 export function buildUserErrorEmbed(type, description) {
+
     return createEmbed({
-        title: "Error",
+
+        title: 'Error',
+
         description,
+
         color: BotConfig.embeds.colors.error
+
     });
+
 }
-
-
-// DEBUG
-console.log("embeds.js loaded: warningEmbed exported");
