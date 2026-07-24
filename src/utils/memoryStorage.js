b2fefbc4a4870ca3acf457cb92c0be5916283ed9
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-24 15:25:43 -0400

    Update memoryStorage.js

--- src/utils/memoryStorage.js
+++ src/utils/memoryStorage.js
@@ -27,3 +27,28 @@ export default {
     has,
     clear
 };
+export class MemoryStorage {
+    constructor() {
+        this.storage = new Map();
+    }
+
+    get(key) {
+        return this.storage.get(key);
+    }
+
+    set(key, value) {
+        this.storage.set(key, value);
+    }
+
+    deleteKey(key) {
+        this.storage.delete(key);
+    }
+
+    has(key) {
+        return this.storage.has(key);
+    }
+
+    clear() {
+        this.storage.clear();
+    }
+}
