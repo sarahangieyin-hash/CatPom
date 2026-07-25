acca201e9d07b559e0f896aad94c42ba0bd720b0
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-24 23:40:00 -0400

    Update finish_mission.js

--- src/interactions/buttons/finish_mission.js
+++ src/interactions/buttons/finish_mission.js
@@ -2,9 +2,10 @@ import { getMission } from '../../utils/missions.js';
 import { addPomp } from '../../utils/points.js';
 
 export default {
+
     customId: 'finish_mission',
 
-    async execute(interaction) {
+    async execute(interaction, client, args) {
 
         if (!interaction.member.permissions.has('Administrator')) {
             return interaction.reply({
@@ -13,12 +14,15 @@ export default {
             });
         }
 
-        const id = interaction.customId.split(':')[1];
+
+        const id = args[0];
+
 
         const mission = await getMission(
             interaction.guild.id,
             id
         );
+
 
         if (!mission) {
             return interaction.reply({
@@ -27,25 +31,29 @@ export default {
             });
         }
 
+
         for (const user of mission.usuarios) {
+
             await addPomp(
                 interaction.guild.id,
                 user,
                 mission.puntos
             );
+
         }
 
-const participantes = mission.usuarios
-    .map(user => `<@${user}>`)
-    .join('\n');
+
+        const participantes = mission.usuarios
+            .map(user => `<@${user}>`)
+            .join('\n');
 
 
-await interaction.reply(
-    `✅ **Misión completada**\n\n` +
-    `👥 Participantes:\n${participantes}\n\n` +
-    `💎 Cada participante recibió **${mission.puntos} Pomp**.`
-    
-);
+        await interaction.reply(
+            `✅ **Misión completada**\n\n` +
+            `👥 Participantes:\n${participantes}\n\n` +
+            `💎 Cada participante recibió **${mission.puntos} Pomp**.`
         );
+
     }
+
 };
