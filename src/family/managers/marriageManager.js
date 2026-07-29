ef51198323d77fb4aac012f99f8420cbce0e8c16
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-29 03:21:52 +0200

    Update marriageManager.js

--- src/family/managers/marriageManager.js
+++ src/family/managers/marriageManager.js
@@ -1,1 +1,88 @@
+import { generateUnionId } from '../utils/helpers.js';
 
+export class MarriageManager {
+
+    constructor(graph) {
+
+        this.graph = graph;
+
+    }
+
+    createUnion(memberIds) {
+
+        const unionId = generateUnionId();
+
+        this.graph.createUnion(
+            unionId,
+            memberIds
+        );
+
+        for (const memberId of memberIds) {
+
+            const person =
+                this.graph.addPerson(memberId);
+
+            person.unionId = unionId;
+
+        }
+
+        return this.graph.getUnion(unionId);
+
+    }
+
+    addMember(unionId, userId) {
+
+        const union =
+            this.graph.getUnion(unionId);
+
+        if (!union) {
+
+            return null;
+
+        }
+
+        if (!union.members.includes(userId)) {
+
+            union.members.push(userId);
+
+        }
+
+        const person =
+            this.graph.addPerson(userId);
+
+        person.unionId = unionId;
+
+        return union;
+
+    }
+
+    removeMember(unionId, userId) {
+
+        const union =
+            this.graph.getUnion(unionId);
+
+        if (!union) {
+
+            return null;
+
+        }
+
+        union.members =
+            union.members.filter(
+                member => member !== userId
+            );
+
+        const person =
+            this.graph.getPerson(userId);
+
+        if (person) {
+
+            person.unionId = null;
+
+        }
+
+        return union;
+
+    }
+
+}
