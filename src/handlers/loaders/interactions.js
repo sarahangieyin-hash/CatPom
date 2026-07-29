63962a009c544cce948bc1094e7af785428d8100
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-29 20:35:44 +0200

    Update interactions.js

--- src/handlers/loaders/interactions.js
+++ src/handlers/loaders/interactions.js
@@ -4,59 +4,105 @@ import { fileURLToPath } from 'url';
 
 
 const __dirname = path.dirname(
-  fileURLToPath(import.meta.url)
+    fileURLToPath(import.meta.url)
 );
 
 
 export default async function loadInteractions(client) {
 
-  const interactionTypes = [
-    {
-      folder: 'buttons',
-      collection: client.buttons
-    },
-    {
-      folder: 'selectMenus',
-      collection: client.selectMenus
+
+    const interactionTypes = [
+
+        {
+            folder: 'buttons',
+            collection: client.buttons
+        },
+
+        {
+            folder: 'selectMenus',
+            collection: client.selectMenus
+        }
+
+    ];
+
+
+
+    for (const type of interactionTypes) {
+
+
+        const folderPath = path.join(
+
+            __dirname,
+
+            '../../interactions',
+
+            type.folder
+
+        );
+
+
+
+        if (!fs.existsSync(folderPath))
+            continue;
+
+
+
+        const files = fs.readdirSync(folderPath)
+
+            .filter(
+                file => file.endsWith('.js')
+            );
+
+
+
+        for (const file of files) {
+
+
+            const interaction =
+                await import(
+
+                    `../../interactions/${type.folder}/${file}`
+
+                );
+
+
+
+            const data =
+                interaction.default;
+
+
+
+            if (
+                !data?.customId ||
+                !data.execute
+            )
+                continue;
+
+
+
+            console.log(
+
+                'INTERACTION REGISTRADA:',
+
+                data.customId
+
+            );
+
+
+
+            type.collection.set(
+
+                data.customId,
+
+                data
+
+            );
+
+
+        }
+
+
     }
-  ];
 
 
-  for (const type of interactionTypes) {
-
-    const folderPath = path.join(
-      __dirname,
-      '../../interactions',
-      type.folder
-    );
-
-
-    if (!fs.existsSync(folderPath))
-      continue;
-
-
-    const files = fs.readdirSync(folderPath)
-      .filter(file => file.endsWith('.js'));
-
-
-    for (const file of files) {
-
-      const interaction = await import(
-        `../../interactions/${type.folder}/${file}`
-      );
-
-
-      const data = interaction.default;
-
-
-      if (!data?.customId || !data.execute)
-        continue;
-
-
-      type.collection.set(
-        data.customId,
-        data
-      );
-    }
-  }
 }
