2a3a40c9429f8ab11cbad6eb98f4ebd393e4ba93
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-29 03:22:16 +0200

    Update familyManager.js

--- src/family/managers/familyManager.js
+++ src/family/managers/familyManager.js
@@ -1,1 +1,93 @@
+export class FamilyManager {
 
+    constructor(graph) {
+
+        this.graph = graph;
+
+    }
+
+    getPerson(userId) {
+
+        return this.graph.getPerson(userId);
+
+    }
+
+    getUnion(unionId) {
+
+        return this.graph.getUnion(unionId);
+
+    }
+
+    getFamilyFromPerson(userId) {
+
+        const person = this.graph.getPerson(userId);
+
+        if (!person) {
+
+            return null;
+
+        }
+
+        if (!person.unionId) {
+
+            return {
+
+                person,
+
+                union: null,
+
+                members: [userId],
+                children: person.children
+
+            };
+
+        }
+
+        const union = this.graph.getUnion(
+            person.unionId
+        );
+
+        return {
+
+            person,
+
+            union,
+
+            members: union?.members ?? [],
+            children: union?.children ?? []
+
+        };
+
+    }
+
+    isInUnion(userId) {
+
+        const person = this.graph.getPerson(userId);
+
+        if (!person) {
+
+            return false;
+
+        }
+
+        return person.unionId !== null;
+
+    }
+
+    getMembers(userId) {
+
+        const family = this.getFamilyFromPerson(userId);
+
+        return family.members;
+
+    }
+
+    getChildren(userId) {
+
+        const family = this.getFamilyFromPerson(userId);
+
+        return family.children;
+
+    }
+
+}
