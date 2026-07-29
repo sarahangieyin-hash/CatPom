424ff468d14d2ed5614f87e05850d964dacafc99
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-29 03:57:51 +0200

    Create theme.js

---
+++ src/family/render/theme.js
@@ -0,0 +1,34 @@
+export const familyTheme = {
+
+    background: '#111111',
+
+
+    marriageLine: '#ffffff',
+
+
+    childLine: '#ffffff',
+
+
+    memberNode: '#ffd166',
+
+
+    childNode: '#7ec8ff',
+
+
+    border: '#ffffff',
+
+
+    text: '#ffffff',
+
+
+    icons: {
+
+        marriage: '💍',
+
+        lover: '🔥',
+
+        child: '👶'
+
+    }
+
+};
