edb31a80f77d1b2b7ba791489385c00333d44a59
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-29 04:57:03 +0200

    Update reject_adoption.js

--- src/interactions/buttons/reject_adoption.js
+++ src/interactions/buttons/reject_adoption.js
@@ -1,1 +1,39 @@
+export default async function(interaction) {
 
+
+    const [
+        ,
+        parentId,
+        childId
+    ] = interaction.customId.split('_');
+
+
+
+    if (
+        interaction.user.id !== childId
+    ) {
+
+        return interaction.reply({
+
+            content:
+                '❌ Esta solicitud no es para ti.',
+
+            ephemeral: true
+
+        });
+
+    }
+
+
+
+    await interaction.update({
+
+        content:
+            `❌ <@${childId}> ha rechazado la adopción de <@${parentId}>.`,
+
+        components: []
+
+    });
+
+
+}
