2ef00f2f9d7a9834f465d391710f73a4c9caad34
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-29 03:21:09 +0200

    Update helpers.js

--- src/family/utils/helpers.js
+++ src/family/utils/helpers.js
@@ -1,1 +1,43 @@
+export function generateUnionId() {
 
+    return `union_${Date.now()}_${Math.floor(Math.random() * 100000)}`;
+
+}
+
+export function unique(array) {
+
+    return [...new Set(array)];
+
+}
+
+export function removeFromArray(array, value) {
+
+    return array.filter(item => item !== value);
+
+}
+
+export function arrayEquals(a, b) {
+
+    if (a.length !== b.length) {
+
+        return false;
+
+    }
+
+    return a.every(value => b.includes(value));
+
+}
+
+export function chunk(array, size) {
+
+    const result = [];
+
+    for (let i = 0; i < array.length; i += size) {
+
+        result.push(array.slice(i, i + size));
+
+    }
+
+    return result;
+
+}
