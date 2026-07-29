6c837bf9873d6550c92c23f248e9b98e0241b6d3
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-29 03:20:23 +0200

    Update validator.js

--- src/family/utils/validator.js
+++ src/family/utils/validator.js
@@ -1,1 +1,43 @@
+import { MAX_CHILDREN } from '../constants.js';
 
+export function canAdopt(person) {
+
+    return person.children.length < MAX_CHILDREN;
+
+}
+
+export function alreadyRelated(person, userId, list) {
+
+    return person[list].includes(userId);
+
+}
+
+export function canMarry(person, userId) {
+
+    return !person.marriages.includes(userId);
+
+}
+
+export function canBecomeLovers(person, userId) {
+
+    return !person.lovers.includes(userId);
+
+}
+
+export function canBecomeEnemies(person, userId) {
+
+    return !person.enemies.includes(userId);
+
+}
+
+export function canAddSibling(person, userId) {
+
+    return !person.siblings.includes(userId);
+
+}
+
+export function canAddParent(person) {
+
+    return person.parents.length < 2;
+
+}
