3e0de6bdee90d5dac4b1deb5cd9d53313cdfbf33
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-29 03:20:09 +0200

    Update graph.js

--- src/family/utils/graph.js
+++ src/family/utils/graph.js
@@ -1,1 +1,56 @@
+export class FamilyGraph {
 
+    constructor() {
+
+        this.people = new Map();
+        this.relationships = [];
+
+    }
+
+    addPerson(userId) {
+
+        if (!this.people.has(userId)) {
+
+            this.people.set(userId, {
+                id: userId,
+                marriages: [],
+                lovers: [],
+                parents: [],
+                children: [],
+                siblings: [],
+                enemies: [],
+                friends: []
+            });
+
+        }
+
+        return this.people.get(userId);
+
+    }
+
+    getPerson(userId) {
+
+        return this.people.get(userId);
+
+    }
+
+    addRelationship(type, from, to) {
+
+        this.addPerson(from);
+        this.addPerson(to);
+
+        this.relationships.push({
+            type,
+            from,
+            to
+        });
+
+    }
+
+    getRelationships() {
+
+        return this.relationships;
+
+    }
+
+}
