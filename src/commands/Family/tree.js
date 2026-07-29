25c86f3becbbb07acec7a15107b04eada4cd3643
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-29 03:53:11 +0200

    Update tree.js

--- src/commands/Family/tree.js
+++ src/commands/Family/tree.js
@@ -1,1 +1,89 @@
+import {
+    SlashCommandBuilder
+} from 'discord.js';
 
+import {
+    getFamilyByMember
+} from '../../utils/families.js';
+
+import {
+    renderFamilyTree
+} from '../../family/render/treeRenderer.js';
+
+
+export default {
+
+    data: new SlashCommandBuilder()
+
+        .setName('tree')
+
+        .setDescription('Muestra el árbol familiar.'),
+
+
+
+    async execute(interaction) {
+
+
+        const family =
+            await getFamilyByMember(
+
+                interaction.guild.id,
+
+                interaction.user.id
+
+            );
+
+
+
+        if (!family) {
+
+            return interaction.reply({
+
+                content:
+                    '❌ No perteneces a ninguna familia.',
+
+                ephemeral: true
+
+            });
+
+        }
+
+
+
+        await interaction.deferReply();
+
+
+
+        const image =
+            await renderFamilyTree(
+
+                interaction.guild,
+
+                family
+
+            );
+
+
+
+        await interaction.editReply({
+
+            files: [
+
+                {
+
+                    attachment:
+                        image,
+
+                    name:
+                        'arbol-familiar.png'
+
+                }
+
+            ]
+
+        });
+
+
+    }
+
+};
