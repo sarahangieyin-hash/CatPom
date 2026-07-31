c2f1a4a970500264258befec7bed54cc00286380
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-31 18:40:05 +0200

    Update fonts.js

--- src/family/render/fonts.js
+++ src/family/render/fonts.js
@@ -1,9 +1,12 @@
+import { registerFont } from 'canvas';
+import path from 'path';
+
+// Intentar registrar una fuente física si la tienes en alguna carpeta local,
+// o configurar una familia estándar de reserva.
+export const fontFamily = 'sans-serif';
+
 export const fonts = {
-
-    title: '40px Arial',
-
-    name: '20px Arial',
-
-    icon: '35px Arial'
-
+    title: `bold 40px ${fontFamily}`,
+    name: `bold 13px ${fontFamily}`,
+    icon: `35px ${fontFamily}`
 };
