1848cdde6921607599070267f12cfca28919fd0c
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-31 20:55:29 +0200

    Update layout.js

--- src/family/render/layout.js
+++ src/family/render/layout.js
@@ -1,11 +1,10 @@
 const NODE_WIDTH = 120;
 const NODE_HEIGHT = 46; 
-const HORIZONTAL_GAP = 90;
-const VERTICAL_GAP = 80;
+const HORIZONTAL_GAP = 60;
+const VERTICAL_GAP = 90;
 
 export async function calculateLayout(guild, family) {
     const rootId = family.userId || family.targetUser;
-    const direction = family.settings?.direction || 'TB';
 
     const fetchUser = async (id) => {
         try {
@@ -52,86 +51,70 @@ export async function calculateLayout(guild, family) {
     for (const id of loverIds) await addNode(id, 1);
     for (const id of childIds) await addNode(id, 2);
 
-    if (direction === 'TB') {
-        const partners = [...spouseIds, ...loverIds];
-        const level1Nodes = [rootNode];
-        for (const pId of partners) {
-            const pNode = nodesMap.get(pId);
-            if (pNode && !level1Nodes.includes(pNode)) level1Nodes.push(pNode);
-        }
+    // --- FILA 1 (CENTRAL): Tú, Parejas y Hermanos ordenados horizontalmente ---
+    const level1Ids = [rootNode.id, ...siblingIds, ...spouseIds, ...loverIds];
+    // Eliminar duplicados si los hubiera
+    const uniqueLevel1 = [...new Set(level1Ids)];
+    
+    const totalWidth = uniqueLevel1.length * NODE_WIDTH + (uniqueLevel1.length - 1) * HORIZONTAL_GAP;
+    let startX = -totalWidth / 2;
 
-        const totalL1Width = level1Nodes.length * NODE_WIDTH + (level1Nodes.length - 1) * HORIZONTAL_GAP;
-        let startX = -totalL1Width / 2;
-
-        level1Nodes.forEach((node) => {
+    uniqueLevel1.forEach((id) => {
+        const node = nodesMap.get(id);
+        if (node) {
             node.x = startX;
             node.y = 0;
             startX += NODE_WIDTH + HORIZONTAL_GAP;
+        }
+    });
+
+    // --- FILA 0 (PADRES): Arriba ---
+    const level0Nodes = parentIds.map(id => nodesMap.get(id)).filter(Boolean);
+    if (level0Nodes.length > 0) {
+        let startX0 = -((level0Nodes.length * NODE_WIDTH + (level0Nodes.length - 1) * HORIZONTAL_GAP) / 2);
+        level0Nodes.forEach((node) => {
+            node.x = startX0;
+            node.y = -(NODE_HEIGHT + VERTICAL_GAP);
+            startX0 += NODE_WIDTH + HORIZONTAL_GAP;
         });
-
-        const level0Nodes = parentIds.map(id => nodesMap.get(id)).filter(Boolean);
-        if (level0Nodes.length === 1) {
-            level0Nodes[0].x = rootNode.x;
-            level0Nodes[0].y = -(NODE_HEIGHT + VERTICAL_GAP);
-        } else if (level0Nodes.length > 0) {
-            const totalL0Width = level0Nodes.length * NODE_WIDTH + (level0Nodes.length - 1) * HORIZONTAL_GAP;
-            let startX0 = -totalL0Width / 2;
-            level0Nodes.forEach((node) => {
-                node.x = startX0;
-                node.y = -(NODE_HEIGHT + VERTICAL_GAP);
-                startX0 += NODE_WIDTH + HORIZONTAL_GAP;
-            });
-        }
-
-        const level2Nodes = childIds.map(id => nodesMap.get(id)).filter(Boolean);
-        if (level2Nodes.length > 0) {
-            const totalL2Width = level2Nodes.length * NODE_WIDTH + (level2Nodes.length - 1) * HORIZONTAL_GAP;
-            let startX2 = -totalL2Width / 2;
-            level2Nodes.forEach((node) => {
-                node.x = startX2;
-                node.y = (NODE_HEIGHT + VERTICAL_GAP);
-                startX2 += NODE_WIDTH + HORIZONTAL_GAP;
-            });
-        }
     }
 
-    // Conexiones de padres
-    if (parentIds.length > 0) {
-        if (parentIds.length === 1) {
-            connections.push({
-                fromNodeId: parentIds[0],
-                toNodeId: rootNode.id,
-                type: 'parent-child-direct'
-            });
-        }
+    // --- FILA 2 (HIJOS): Abajo ---
+    const level2Nodes = childIds.map(id => nodesMap.get(id)).filter(Boolean);
+    if (level2Nodes.length > 0) {
+        let startX2 = -((level2Nodes.length * NODE_WIDTH + (level2Nodes.length - 1) * HORIZONTAL_GAP) / 2);
+        level2Nodes.forEach((node) => {
+            node.x = startX2;
+            node.y = (NODE_HEIGHT + VERTICAL_GAP);
+            startX2 += NODE_WIDTH + HORIZONTAL_GAP;
+        });
     }
 
-    // Conexión de pareja unificada (en lugar de una por cada cónyuge suelto)
-    const partners = [...spouseIds, ...loverIds];
-    if (partners.length > 0) {
+    // --- CONEXIONES ---
+    parentIds.forEach(pId => {
+        connections.push({
+            fromNodeId: pId,
+            toNodeId: rootNode.id,
+            type: 'parent-child-direct'
+        });
+    });
+
+    const allPartners = [...spouseIds, ...loverIds];
+    allPartners.forEach(pId => {
         connections.push({
             fromNodeId: rootNode.id,
-            partnerIds: partners,
-            type: 'partners-group'
+            toNodeId: pId,
+            type: 'partner'
         });
-    }
+    });
 
-    // Conexiones de hijos
     childIds.forEach(cId => {
-        if (partners.length > 0) {
-            connections.push({
-                fromNodeId: rootNode.id,
-                toNodeId: cId,
-                type: 'family-child',
-                partnerId: partners[0]
-            });
-        } else {
-            connections.push({
-                fromNodeId: rootNode.id,
-                toNodeId: cId,
-                type: 'parent-child-direct'
-            });
-        }
+        connections.push({
+            fromNodeId: rootNode.id,
+            toNodeId: cId,
+            type: 'family-child',
+            partnerId: allPartners[0] || null
+        });
     });
 
     return {
