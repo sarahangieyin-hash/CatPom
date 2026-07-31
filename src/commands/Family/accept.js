4f144d70437697c5fbfcf4ec5c1bb1905440cee1
sarahangieyin-hash <sarahangieyin@gmail.com>
2026-07-31 04:14:59 +0200

    Update accept.js

--- src/commands/Family/accept.js
+++ src/commands/Family/accept.js
@@ -1,1 +1,20 @@
+import { SlashCommandBuilder } from 'discord.js';
 
+export default {
+    data: new SlashCommandBuilder()
+        .setName('accept')
+        .setDescription('Acepta una solicitud familiar mediante ID.')
+        .addStringOption(option =>
+            option
+                .setName('id')
+                .setDescription('ID de la solicitud')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        await interaction.reply({
+            content: 'ℹ️ Usa los botones de los mensajes para aceptar o rechazar solicitudes.',
+            ephemeral: true
+        });
+    }
+};
